a2c24313a0aad1ce7139329a43f63ee145e11862
Togrul Salmanli <togrulsalmanli33@gmail.com>
2020-08-17 11:42:01 +0400

    Ilk Isim

---
+++ js/home.js
@@ -0,0 +1,94 @@
+$(function(){
+    'use strict';
+	$(".H5_1").click(function(){
+		$(".UL_HOME_1").toggle()
+	})
+})
+
+
+$(function(){
+    'use strict';
+	$(".H5_2").click(function(){
+		$(".UL_HOME_2").toggle()
+	})
+})
+
+
+$(function(){
+    'use strict';
+	$(".H5_3").click(function(){
+		$(".UL_HOME_3").toggle()
+	})
+})
+
+
+$(function(){
+    'use strict';
+	$(".H5_4").click(function(){
+		$(".UL_HOME_4").toggle()
+	})
+})
+
+
+
+$(function(){
+    'use strict';
+	$(".H5_5").click(function(){
+		$(".UL_HOME_5").toggle()
+	})
+})
+
+
+$(function(){
+    'use strict';
+	$(".H5_6").click(function(){
+		$(".UL_HOME_6").toggle()
+	})
+})
+
+
+$(function(){
+    'use strict';
+	$(".H5_7").click(function(){
+		$(".UL_HOME_7").toggle()
+	})
+})
+
+
+$(function(){
+    'use strict';
+	$(".H5_8").click(function(){
+		$(".UL_HOME_8").toggle()
+	})
+})
+
+
+$(function(){
+    'use strict';
+	$(".H5_9").click(function(){
+		$(".UL_HOME_9").toggle()
+	})
+})
+
+$(function(){
+    'use strict';
+	$(".H5_10").click(function(){
+		$(".UL_HOME_10").toggle()
+	})
+})
+
+
+$(function(){
+    'use strict';
+	$(".H5_11").click(function(){
+		$(".UL_HOME_11").toggle()
+	})
+})
+
+
+
+
+
+
+
+
